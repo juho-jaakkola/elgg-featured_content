6886ff054b4b31cf02bc95f43490ae4559e66a9b
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-02-13 11:49:06 +0200

    Allows only admins and publishers to unfeature content

--- actions/featured_content/unfeature.php
+++ actions/featured_content/unfeature.php
@@ -4,6 +4,7 @@ $user = elgg_get_logged_in_user_entity();
 
 if (!$user->isAdmin() && $user->role !== 'publisher') {
 	register_error(elgg_echo('actionunauthorized'));
+	forward(REFERER);
 }
 
 $guid = get_input('guid');
